cd5f872cdef89fbd4e106de9f6ced8ab55ab19f5
joelpalacio <palaciojoel855@gmail.com>
2025-08-27 15:34:14 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0q4vuCWDeqvpuLjiLGHLgFB-ZMBXsgqg",
+  authDomain: "cjd-goal.firebaseapp.com",
+  projectId: "cjd-goal",
+  storageBucket: "cjd-goal.firebasestorage.app",
+  messagingSenderId: "416642936646",
+  appId: "1:416642936646:web:6a3448bfeb44beccb27f47"
+};
+
+
+const app = initializeApp(firebaseConfig);
